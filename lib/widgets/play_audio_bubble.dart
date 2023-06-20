52d31e9c6736838ffef77df9092a7480fe2699ea
yungchien <yungchien@locas.com.tw>
2023-06-20 16:00:52 +0800

    0620update

---
+++ lib/widgets/play_audio_bubble.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PlayAudioBubble extends StatefulWidget {
+  const PlayAudioBubble({Key? key}) : super(key: key);
+
+  @override
+  State<PlayAudioBubble> createState() => _PlayAudioBubbleState();
+}
+
+class _PlayAudioBubbleState extends State<PlayAudioBubble> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
