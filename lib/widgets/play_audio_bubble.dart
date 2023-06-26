51a4d48b82c1371bc50802834361f8155dbdfb13
yungchien <yungchien@locas.com.tw>
2023-06-26 18:20:54 +0800

    0626update_建置訊息聲波紋UI

--- lib/widgets/play_audio_bubble.dart
+++ lib/widgets/play_audio_bubble.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:dio/dio.dart';
-import 'package:just_waveform/just_waveform.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:base_project/constant/theme/app_image_path.dart';
+import 'package:flutter/foundation.dart';
 import 'package:base_project/constant/theme/app_text_style.dart';
-import 'package:base_project/constant/theme/global_data.dart';
 import 'package:base_project/constant/theme/ui_define.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,7 +13,7 @@ import '../constant/theme/app_gradient_colors.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import 'package:just_audio/just_audio.dart';
-import 'audio_wave_form_widget.dart';
+import 'package:audioplayers/audioplayers.dart' as audio;
 
 class PlayAudioBubble extends StatefulWidget {
   final String path;
@@ -36,10 +33,40 @@ class _PlayAudioBubbleState extends State<PlayAudioBubble> {
   Duration? currentPosition;
 
 
-@override
+  late Duration maxDuration;
+  late Duration elapsedDuration;
+  late List<String> audioData;
+  late AudioCache audioPlayer;
+  late int totalSamples;
+
+  Future<void> parseData() async {
+    final json = await rootBundle.loadString(audioData[0]);
+    Map<String, dynamic> audioDataMap = {
+      "json": json,
+      "totalSamples": totalSamples,
+    };
+    await audioPlayer.load(audioData[1]);
+    // maxDuration in milliseconds
+    await Future.delayed(const Duration(milliseconds: 200));
+
+    int maxDurationInmilliseconds =
+    await audioPlayer.fixedPlayer!.getDuration();
+
+    maxDuration = Duration(milliseconds: maxDurationInmilliseconds);
+    setState(() {
+      samples = samplesData["samples"];
+    });
+  }
+
+  @override
   void initState() {
   Future.delayed(Duration.zero,()async{
-     duration = await player.setUrl(widget.path);
+    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+    final timeStamp = DateTime.now().millisecondsSinceEpoch.toString();
+    await downloadFile(widget.path, '$appDocumentsDir/$timeStamp/msg.wav');
+
+
+    duration = await player.setUrl(widget.path);
      player.positionStream.listen((position) {
        print('position=$position');
        setState(() {
@@ -89,86 +116,86 @@ void dispose() {
       child: Row(
         children: [
           SizedBox(width: UIDefine.getPixelWidth(5),),
-          StreamBuilder<PlayerState>(
-            stream: player.playerStateStream,
-            builder: (context, snapshot) {
-              final playerState = snapshot.data;
-              final processingState = playerState?.processingState;
-              final playing = playerState?.playing;
-              if (processingState == ProcessingState.loading ||
-                  processingState == ProcessingState.buffering) {
-                return Container(
-                  width: UIDefine.getPixelWidth(30),
-                  height: UIDefine.getPixelWidth(30),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
-                    color: AppColors.buttonAudio.getColor().withOpacity(0.2)
-                  ),
-                  child:Image.asset(AppImagePath.blackPlayIcon),
-                );
-              } else if (playing != true) {
-                return GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      isPlaying = true;
-                      if(isPaused){
-                        isPaused = false;
-                      }
-                      player.play();
-                    });
-                  },
-                  child: Container(
-                    width: UIDefine.getPixelWidth(30),
-                    height: UIDefine.getPixelWidth(30),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
-                        color: AppColors.buttonAudio.getColor().withOpacity(0.2)
-                    ),
-                    child:Image.asset(AppImagePath.blackPlayIcon),
-                  ),
-                );
-              } else if (processingState != ProcessingState.completed) {
-                return GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      isPlaying = false;
-                      isPaused = true;
-                      player.pause();
-                    });
-
-                  },
-                  child: Container(
-                    width: UIDefine.getPixelWidth(30),
-                    height: UIDefine.getPixelWidth(30),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
-                        color: AppColors.buttonAudio.getColor().withOpacity(0.2)
-                    ),
-                    child:Image.asset(AppImagePath.pauseBlackIcon),
-                  ),
-                );
-              } else {
-                return
-                  GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        isPlaying = true;
-                        player.seek(Duration.zero);
-                      });
-                    },
-                    child: Container(
-                      width: UIDefine.getPixelWidth(30),
-                      height: UIDefine.getPixelWidth(30),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
-                          color: AppColors.buttonAudio.getColor().withOpacity(0.2)
-                      ),
-                      child:Image.asset(AppImagePath.blackPlayIcon),
-                    ),
-                  );
-              }
-            },
-          ),
+          // StreamBuilder<PlayerState>(
+          //   stream: player.playerStateStream,
+          //   builder: (context, snapshot) {
+          //     final playerState = snapshot.data;
+          //     final processingState = playerState?.processingState;
+          //     final playing = playerState?.playing;
+          //     if (processingState == ProcessingState.loading ||
+          //         processingState == ProcessingState.buffering) {
+          //       return Container(
+          //         width: UIDefine.getPixelWidth(30),
+          //         height: UIDefine.getPixelWidth(30),
+          //         decoration: BoxDecoration(
+          //           borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
+          //           color: AppColors.buttonAudio.getColor().withOpacity(0.2)
+          //         ),
+          //         child:Image.asset(AppImagePath.blackPlayIcon),
+          //       );
+          //     } else if (playing != true) {
+          //       return GestureDetector(
+          //         onTap: (){
+          //           setState(() {
+          //             isPlaying = true;
+          //             if(isPaused){
+          //               isPaused = false;
+          //             }
+          //             player.play();
+          //           });
+          //         },
+          //         child: Container(
+          //           width: UIDefine.getPixelWidth(30),
+          //           height: UIDefine.getPixelWidth(30),
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
+          //               color: AppColors.buttonAudio.getColor().withOpacity(0.2)
+          //           ),
+          //           child:Image.asset(AppImagePath.blackPlayIcon),
+          //         ),
+          //       );
+          //     } else if (processingState != ProcessingState.completed) {
+          //       return GestureDetector(
+          //         onTap: (){
+          //           setState(() {
+          //             isPlaying = false;
+          //             isPaused = true;
+          //             player.pause();
+          //           });
+          //
+          //         },
+          //         child: Container(
+          //           width: UIDefine.getPixelWidth(30),
+          //           height: UIDefine.getPixelWidth(30),
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
+          //               color: AppColors.buttonAudio.getColor().withOpacity(0.2)
+          //           ),
+          //           child:Image.asset(AppImagePath.pauseBlackIcon),
+          //         ),
+          //       );
+          //     } else {
+          //       return
+          //         GestureDetector(
+          //           onTap: (){
+          //             setState(() {
+          //               isPlaying = true;
+          //               player.seek(Duration.zero);
+          //             });
+          //           },
+          //           child: Container(
+          //             width: UIDefine.getPixelWidth(30),
+          //             height: UIDefine.getPixelWidth(30),
+          //             decoration: BoxDecoration(
+          //                 borderRadius: BorderRadius.circular(UIDefine.getPixelWidth(15)),
+          //                 color: AppColors.buttonAudio.getColor().withOpacity(0.2)
+          //             ),
+          //             child:Image.asset(AppImagePath.blackPlayIcon),
+          //           ),
+          //         );
+          //     }
+          //   },
+          // ),
           SizedBox(width: UIDefine.getPixelWidth(10),),
           isPlaying == true || isPaused == true?
           Text(currentPosition.toString().substring(2,7),style: AppTextStyle.getBaseStyle(color: AppColors.textBlack,fontSize: UIDefine.fontSize14,fontWeight: FontWeight.w400),):
