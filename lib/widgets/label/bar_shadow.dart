ad79fd581b5ab1ebd5a2f0a7d2b84802e55ea861
fangchi <lily@locas.com.tw>
2023-07-03 17:17:41 +0800

    1.陰影調整回原樣

--- lib/widgets/label/bar_shadow.dart
+++ lib/widgets/label/bar_shadow.dart
@@ -10,17 +10,32 @@ class BarShadow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      height: height ?? UIDefine.getPixelWidth(91),
+      width: UIDefine.getWidth(),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.bottomCenter,
+              colors: [
+            AppColors.textBlack.getColor().withOpacity(0.5),
+            AppColors.textBlack.getColor().withOpacity(0),
+          ])),
+    );
+
     return GlassContainer(
       height: height ?? UIDefine.getPixelWidth(120),
       width: UIDefine.getWidth(),
       border: 0.0,
       blur: 0.7,
       radius: 0,
-        linearGradient: LinearGradient(
-              begin: Alignment.center,
-              end: Alignment.bottomCenter,
-              colors: [
-            AppColors.textBlack.getColor().withOpacity(0.5),
+      linearGradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            AppColors.textBlack.getColor().withOpacity(0.6),
+            AppColors.textBlack.getColor().withOpacity(0.6),
+            AppColors.textBlack.getColor().withOpacity(0.078),
             AppColors.textBlack.getColor().withOpacity(0),
           ]),
     );
