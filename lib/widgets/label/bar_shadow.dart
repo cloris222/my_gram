a419c1569cfd17445c81af2009ddf9aa3d6eac00
fangchi <lily@locas.com.tw>
2023-07-03 16:59:53 +0800

    1.陰影&動態的日期調整

---
+++ lib/widgets/label/bar_shadow.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:glassmorphism_widgets/glassmorphism_widgets.dart';
+
+import '../../constant/theme/app_colors.dart';
+import '../../constant/theme/ui_define.dart';
+
+class BarShadow extends StatelessWidget {
+  const BarShadow({Key? key, this.height}) : super(key: key);
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassContainer(
+      height: height ?? UIDefine.getPixelWidth(120),
+      width: UIDefine.getWidth(),
+      border: 0.0,
+      blur: 0.7,
+      radius: 0,
+        linearGradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            AppColors.textBlack.getColor().withOpacity(0.6),
+                AppColors.textBlack.getColor().withOpacity(0.6),
+                AppColors.textBlack.getColor().withOpacity(0.078),
+            AppColors.textBlack.getColor().withOpacity(0),
+          ]),
+    );
+  }
+}
